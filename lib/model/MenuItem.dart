a187d0f7985109ecbab6e4cfaf39b0621a28c6e9
ksingh0983 <singh144421>
2019-10-05 18:47:20 -0400

    Update Manu Layout

--- lib/model/MenuItem.dart
+++ lib/model/MenuItem.dart
@@ -12,19 +12,68 @@ class MenuItem extends StatelessWidget{
 
   MenuItem(Map<String, dynamic> data){
 
-    name = data["Name"];
-    category = data["Category"];
-    description = data["Description"];
-    price = data["Price"];
-    spiceLvl = data["SpiceLvl"];
-    favorite = data["Favorite"];
+    name = data["name"];
+    category = data["category"];
+    description = data["description"];
+    price = data["price"];
+    spiceLvl = data["spiceLvl"];
+    favorite = data["favorite"];
+
 
   }
 
   @override
   Widget build(BuildContext context) {
-    return new Text(name);
-    //return new Column()
-  }
+    //return new Text(name);
+    //return new Column( children:<Widget>[new Text(name),new Text(price.toString())]);
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              width: 100.0,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image:AssetImage('lib/assets/images/samosa.jpg'),
+                fit: BoxFit.cover
+                ),
+                borderRadius: BorderRadius.only(topRight: Radius.circular(10.0),bottomRight: Radius.circular(10.0),)
+              ),
+            ),
+            SizedBox(width: 10.0),
+            Container(
+              height: 100,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(name,style: TextStyle(
+                    fontSize: 15.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold
+                  ),),
+                  SizedBox(height: 5.0,),
+                  Text(description,style: TextStyle(
+                    fontSize: 11.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey
+                  ),),
+                  SizedBox(height: 10.0),
+                  new Text(price.toString(),style: TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Montserrat',
+                    fontWeight: FontWeight.bold,
+                      color:Colors.grey
+                  ),)
 
+                ],
+              ),
+            )
+          ],
+        )
+      ],
+    );
+
+  }
 }
